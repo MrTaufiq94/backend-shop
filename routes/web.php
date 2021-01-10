56b506323ad7e53f5b6f09a4c809348f450c0639
MrTaufiq94 <m.taufiqfariduddin94@gmail.com>
2021-01-10 10:44:59 +0800

    backend-shop:admin_module - crud product:index

--- routes/web.php
+++ routes/web.php
@@ -39,5 +39,8 @@ Route::prefix('admin')->group(function () {
 
         //route category
         Route::resource('/category', CategoryController::class, ['as' => 'admin']);
+
+        //route product
+        Route::resource('/product', ProductController::class, ['as' => 'admin']);
     });
 });
