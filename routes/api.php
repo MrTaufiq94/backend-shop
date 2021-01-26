fede894eddc057fdc1d1fc40fc0a4fc2ab5dfd51
MrTaufiq94 <m.taufiqfariduddin94@gmail.com>
2021-01-27 00:03:13 +0800

    backend-shop:admin_module - create api:route order

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login'])->name('api.customer.login');
 Route::post('/register', [AuthController::class, 'register'])->name('api.customer.register');
 Route::get('/user', [AuthController::class, 'getUser'])->name('api.customer.user');
+
+/**
+ * Router Order
+ */
+Route::get('/order', [OrderController::class, 'index'])->name('api.order.index');
+Route::get('/order/{snap_token?}', [OrderController::class, 'show'])->name('api.order.show');
